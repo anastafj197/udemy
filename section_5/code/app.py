1708c98e8f75d9fa9ef39062e437a2806a050728
Frances Anastasia <frankie.anastasia@gmail.com>
2020-08-25 15:59:03 -0400

    Reterival from sqlite DB

--- section_5/code/app.py
+++ section_5/code/app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request 
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required 
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT 
 
+from user import UserRegister
 from security import authenticate, identity
+
+from item import Item, ItemList 
+
 
 app = Flask(__name__)
 
@@ -13,59 +17,8 @@ api = Api(app)
 # JWT creates a new endpoint /auth -> sent username and password 
 jwt = JWT(app, authenticate, identity)
 
-items = []
-
-class Item(Resource):
-	parser = reqparse.RequestParser()
-	parser.add_argument('price',
-		type = float,
-		required = True,
-		help = "This field cannot be left blank!"
-	)
-
-	@jwt_required()
-	def get(self, name):
-		# Gives first item found by this filter function 
-		# filter function takes two arguments, 1 filtering function, 2 list of items you want to filter 
-		# go through each item execute this function and see if the items name x name matches -> return 
-		item = next(filter(lambda x: x['name'] == name, items), None) # Does item named x match the parameter 
-		return {'item' : item}, 200 if item else 404 # 200 if item exists if not 404
-
-	# Error first 
-	def post(self, name):
-		if next(filter(lambda x: x['name'] == name, items), None):
-			return {'message': "An item with that name '{}' already exists.".format(name)}, 400
-
-		data = Item.parser.parse_args()
-
-		item = {'name': name, 'price': data['price']}
-		items.append(item)
-		return item, 201
-
-	def delete(self, name):
-		global items
-		# the DB items = result of lambda -> loop through items add every entry where item's name is not = to the name passed in 
-		items = list(filter(lambda x: x['name'] != name, items))
-		return {'message': 'Item deleted'}
-
-	# The API will not accept any other argument besides price even if they exist inside payload
-	def put(self, name):
-		
-		data = Item.parser.parse_args()
-
-		item = next(filter(lambda x: x['name'] == name, items), None)
-		if item is None:
-			item = {'name': name, 'price': data['price']}
-			items.append(item)
-		else: 
-			item.update(data)
-		return item
-
-class ItemList(Resource):
-	def get(self):
-		return {'items': items}
-
 api.add_resource(Item, '/item/<string:name>') 
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5000, debug=True)
