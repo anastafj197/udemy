608cef22197407184ead8d4d036c412e178e34c8
Frances Anastasia <frankie.anastasia@gmail.com>
2020-09-01 16:32:52 -0400

    Adding stores

---
+++ section_6/code/resources/store.py
@@ -0,0 +1,15 @@
+from flask_restful import Resource
+from models.store import StoreModel 
+
+class Store(Resource):
+	def get(self, name):
+		pass
+
+	def post(self, name):
+		pass
+
+	def delete(self, name):
+		pass
+
+class StoreList(Resource):
+	pass
