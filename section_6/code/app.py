f629dc3620052c1ce4e34217dc0ed893442c2353
Frances Anastasia <frankie.anastasia@gmail.com>
2020-09-04 15:54:37 -0400

    Added the StoreList resource

--- section_6/code/app.py
+++ section_6/code/app.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT 
 
+from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
-from security import authenticate, identity
+from resources.store import Store, StoreList
 
 from db import db
 
@@ -23,6 +24,10 @@ jwt = JWT(app, authenticate, identity) # /auth
 
 api.add_resource(Item, '/item/<string:name>') 
 api.add_resource(ItemList, '/items')
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
